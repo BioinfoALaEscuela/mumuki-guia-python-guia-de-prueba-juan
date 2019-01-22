ae2aae663d07f6717090efec899d45c4500f1718
Juan Vazquez <Juan Vazquez>
2019-01-22 22:12:48 +0000

    Mumuki Export on 2019-01-22 22:12:48 +0000

--- 00001_Un experto Pythonista - Prueba/extra.py
+++ 00001_Un experto Pythonista - Prueba/extra.py
@@ -8,13 +8,15 @@ try:
 except ImportError:
     # Python 3
     import builtins as __builtin__
-    
+
+global result
+
 def print(*args, **kwargs):
     """My custom print() function."""
     # Adding new arguments to the print function signature 
     # is probably a bad idea.
     # Instead consider testing if custom argument keywords
     # are present in kwargs
-    global result = args
+    result = *args + **kwargs
     __builtin__.print('My overridden print() function!')
     return __builtin__.print(*args, **kwargs)
