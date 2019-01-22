2e367a01851121f6b758cac858c91cb4d1ba3761
Juan Vazquez <Juan Vazquez>
2019-01-22 22:27:49 +0000

    Mumuki Export on 2019-01-22 22:27:49 +0000

--- 00001_Un experto Pythonista - Prueba/test.py
+++ 00001_Un experto Pythonista - Prueba/test.py
@@ -3,9 +3,10 @@ class Test(unittest.TestCase):
     try:
       output
       output_exists = True
+      print('output exists!')
     except NameError:
       output_exists = False
-      
+      print('output does not exist!')
     if output_exists:
       print(output)
       self.assertTrue(output == 'Hola Bioinformatica!!')
