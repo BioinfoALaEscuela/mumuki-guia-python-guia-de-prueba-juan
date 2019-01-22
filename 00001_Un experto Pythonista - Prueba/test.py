b0661858626c007d229e610510f75cec246e1713
Juan Vazquez <Juan Vazquez>
2019-01-22 22:26:05 +0000

    Mumuki Export on 2019-01-22 22:26:05 +0000

--- 00001_Un experto Pythonista - Prueba/test.py
+++ 00001_Un experto Pythonista - Prueba/test.py
@@ -1,3 +1,10 @@
 class Test(unittest.TestCase):
   def test_description_example(self):
-    self.assertTrue(output == 'Hola Bioinformatica!!')
+    try:
+      output
+    except NameError:
+      output_exists = False
+      
+    if output_exists:
+      print(output)
+      self.assertTrue(output == 'Hola Bioinformatica!!')
