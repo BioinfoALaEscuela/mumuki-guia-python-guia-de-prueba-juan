c93a8708272a912504b3c63ca05f1b65d46ac1fa
Juan Vazquez <Juan Vazquez>
2019-01-22 22:15:24 +0000

    Mumuki Export on 2019-01-22 22:15:24 +0000

--- 00001_Un experto Pythonista - Prueba/extra.py
+++ 00001_Un experto Pythonista - Prueba/extra.py
@@ -1,8 +1,3 @@
-from __future__ import print_function
-# This must be the first statement before other statements.
-# You may only put a quoted or triple quoted string, 
-# Python comments, other future statements, or blank lines before the __future__ line.
-
 try:
     import __builtin__
 except ImportError:
