3231e170ed7106efe2f89befee4c0dc1ea1bc1b5
Juan Vazquez <Juan Vazquez>
2019-01-22 22:12:04 +0000

    Mumuki Export on 2019-01-22 22:12:04 +0000

--- 00001_Un experto Pythonista - Prueba/extra.py
+++ 00001_Un experto Pythonista - Prueba/extra.py
@@ -1,4 +1,20 @@
-def print(string):
-  global result = string
-  super.print(string)
-  return result
+from __future__ import print_function
+# This must be the first statement before other statements.
+# You may only put a quoted or triple quoted string, 
+# Python comments, other future statements, or blank lines before the __future__ line.
+
+try:
+    import __builtin__
+except ImportError:
+    # Python 3
+    import builtins as __builtin__
+    
+def print(*args, **kwargs):
+    """My custom print() function."""
+    # Adding new arguments to the print function signature 
+    # is probably a bad idea.
+    # Instead consider testing if custom argument keywords
+    # are present in kwargs
+    global result = args
+    __builtin__.print('My overridden print() function!')
+    return __builtin__.print(*args, **kwargs)
