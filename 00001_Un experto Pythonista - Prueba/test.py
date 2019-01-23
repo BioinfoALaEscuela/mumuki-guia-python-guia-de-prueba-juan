909f83672cf42fd485af74b8c85aa7fcbb36940d
Juan Vazquez <Juan Vazquez>
2019-01-23 21:31:57 +0000

    Mumuki Export on 2019-01-23 21:31:57 +0000

--- 00001_Un experto Pythonista - Prueba/test.py
+++ 00001_Un experto Pythonista - Prueba/test.py
@@ -1,20 +1,21 @@
+# importo librerías necesarias
 import sys
-from cStringIO import StringIO
+from cStringIO import StringIO 
 
-# setup the environment
+# guardo el stdout existente para restaurarlo después
 backup = sys.stdout
 
-# ####
-sys.stdout = StringIO()     # capture output
-/*...content...*/
-output = sys.stdout.getvalue() # release output
-# ####
-output = output.strip()
-sys.stdout.close()  # close the stream 
-sys.stdout = backup # restore original stdout
+sys.stdout = StringIO()     # capturo el output
+/*...content...*/           # esto es la interpolación, acá está el código que el alumno ponga en la solución.
 
+output = sys.stdout.getvalue() # guardo lo que salió en consola, o sea en stdout, en la variable output
+
+output = output.strip()    # acá le sacó caracteres extraños como el \n de nueva línea que se agrega al capturar el stdout
+sys.stdout.close()  # cierro la captura
+sys.stdout = backup # restauro el stdout original
+
+#Ahora abajo defino el unit test que prueba si esta todo bien o no.
 class Test(unittest.TestCase):
   def test_description_example(self):
-    print "hola"
-    self.assertTrue(output == 'Hola Bioinformatica!!', 'Tu solucion no esta bien. La salida obtenida es {0}'.format(output))
+    self.assertTrue(output == 'Hola Bioinformatica!!', 'Tu solucion no esta bien. La salida obtenida es {0}'.format(output)) #esta línea devuelve True si output es == a 'Hola Bioinformatica!!' y lo que está después de la coma si no.
       
